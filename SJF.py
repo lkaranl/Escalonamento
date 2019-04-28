a48d1e129b898a65df288dc9b9fa42e14c2c88c2
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2019-04-28 17:51:56 -0400

    Update SJF.py

--- SJF.py
+++ SJF.py
@@ -1,12 +1,32 @@
+# encoding: utf-8
+
 #####	NAME:				ALGORITMO DE ESCALONAMENTO SHORTEST JOB FIRST (SJF)
 #####	VERSION:			1.0
-#####	DESCRIPTION:			O ALGORITMO APRESENTA A ORDEM DA EXECUCAO DOS PROCESSOS, CONFORME ESTRATEGIA DE ESCALONAMENTO
-#####	DATE OF CREATION:		27/04/2019
+#####	DESCRIPTION:		O ALGORITMO APRESENTA A ORDEM DA EXECUCAO DOS PROCESSOS, CONFORME ESTRATEGIA DE ESCALONAMENTO
+#####	DATE OF CREATION:	27/04/2019
 #####	WRITTEN BY:			KARAN LUCIANO SILVA | JACKSON DURAES
 #####	E-MAIL:				karanluciano1@gmail.com			
 #####	DISTRO:				MANJARO LINUX
 #####	LICENSE:			GPLv3 			
 #####	PROJECT:			https://github.com/lkaranl/Vector_Clock
+
+#█████████████████████████████████████
+#████ ▄▄▄▄▄ █ ▀▀▄ ▀ ██ ██▀█ ▄▄▄▄▄ ████
+#████ █   █ ███ ▄▄ ▀ █  ▄▄█ █   █ ████
+#████ █▄▄▄█ █ ▄▄ █ ▄█▀▄▄█▀█ █▄▄▄█ ████
+#████▄▄▄▄▄▄▄█ █ ▀ █ ▀▄█ ▀▄█▄▄▄▄▄▄▄████
+#████   █▀▄▄▀▄  ██▀█▄ ▀█▀    ▄██  ████
+#██████▀█▄▀▄▄ ▀█▄██  ▀██▄ ▀▄▄  ▄█▄████
+#████▄█▄ ▀▀▄▀▀ █▄  ▄█▀▀█▀ █  ███▀ ████
+#████ ▄▄█▄▀▄█ ▄▄▀ █▄ ▄▄█ ▀▄  ▄███▄████
+#████▄▄██▄ ▄▀  ▄██ ▄ ▀▀▄▀▄▄ ▀██▀▀ ████
+#████▄▄█▄▀▄▄▀ ▄ ▄█▀█ ▀█▀▄▀█▀█ ▄██▄████
+#████▄▄▄█▄▄▄█▀▀█▄ ▄█▀ ▄▀█ ▄▄▄  ▀▄▀████
+#████ ▄▄▄▄▄ █▀▀▀▀ ▄ ▄ ██▀ █▄█ ███▄████
+#████ █   █ ██ ▀██   ▀██   ▄▄  ▀▄▀████
+#████ █▄▄▄█ █ ▄█▄█▀█ █▄▀█▀▄█▀ ▀█▄▄████
+#████▄▄▄▄▄▄▄█▄██▄▄▄█▄▄███▄███▄▄██▄████
+#█████████████████████████████████████
 
 #############BIBLIOTECAS
 import random#BIBIOTECA PARA GERAR NUMEROS ALEATORIOS
@@ -24,24 +44,23 @@ Main['bg'] = 'white'#DA A COR
 Main.geometry("400x853")#TAMANHO
 Main.title("Sistemas Operacionais - SJF")#TITULO DA JANELA
 
-
 #############TRATAMENTO
 #VERIFICA SE EXISTE O ARQUIVO NO CAMINHO, SE EXISTIR ELE EH APAGADO
-for raiz, diretorio, arquivos in os.walk('LOCAL EM QUE ESTA LOCALIZADO SEU ARQUIVO SJF.txt BAIXADO'):
+for raiz, diretorio, arquivos in os.walk('/home/karan/Documentos/operacionais'):
 		for arquivo in arquivos:#FOR ARQUIVO EM ARQUIVOS
 			if arquivo.endswith('SJF.txt'):#SE ENCONTRAR O ARQUIVO
 				os.remove(os.path.join(raiz,arquivo))#O ARQUIVO EH APAGADO
 
-
 #############FUNCOES 
-#FUNCAO 'Scan'
+#FUNCAO QUE LISTA OS PROCESSOS E OS CPUBUSTER
+# class _Scan:
 def Scan():
 	contador = 0#CRIA UM CONTADOR
 	numeros = []#CRIA UMA ARRAY
 	pro = int(ETY_PRO.get())#PEGA A INFORMACAO DA LABEL
 
 	#VERIFICA SE EXISTE O ARQUIVO NO CAMINHO, SE EXISTIR ELE EH APAGADO
-	for raiz, diretorio, arquivos in os.walk('LOCAL EM QUE ESTA LOCALIZADO SEU ARQUIVO SJF.txt BAIXADO'):
+	for raiz, diretorio, arquivos in os.walk('/home/karan/Documentos/operacionais'):
 		for arquivo in arquivos:#FOR ARQUIVO EM ARQUIVOS
 			if arquivo.endswith('SJF.txt'):#SE ENCONTRAR O ARQUIVO
 				os.remove(os.path.join(raiz,arquivo))#O ARQUIVO EH APAGADO
@@ -61,30 +80,69 @@ def Scan():
 			arquivo = open('SJF.txt', 'a')#ABRE O ARQUIVO 'SJF.txt' COM PERMISSAO DE LEITARA E ESCRITA
 			arquivo.write("P%s --------------------> %s\n" %(contador,item))#ESCREVE TUDO O QUE ESTA EM 'contador' E 'item'
 			arquivo.close()#FECHA O ARQUIVO
+			
+			arquivo = open('SJF.txt', 'r')#ABRE O ARQUIVO 'STF.txt' COM PERMISSAO DE LEITURA
+			leitura = arquivo.read()#LE TUDO QUE ESTA NO ARQUIVO
+			LBL_Leitura['text'] = leitura#TROCA A CHAVE 'text' DO LABEL 'LBL_Leitura'
+			arquivo.close()#FECHA O ARQUIVO
 
-#FUNCAO 'Lei'
-def Lei():
-	arquivo = open('SJF.txt', 'r')#ABRE O ARQUIVO 'STF.txt' COM PERMISSAO DE LEITURA
-	leitura = arquivo.read()#LE TUDO QUE ESTA NO ARQUIVO
-	LBL_Leitura['text'] = leitura#TROCA A CHAVE 'text' DO LABEL 'LBL_Leitura'
-	arquivo.close()#FECHA O ARQUIVO
+	#PARTE QUE FAZ O CALCULO MEDIO SJF DOS PROCESSOS
+	escolhido = 0
+	y = 1
+	n = int(ETY_PRO.get())
+	tmpExe = []
+	tmpEnt = []
 
+	for x in xrange(1,n+1):
+		tmpEnt.append(float(y))
+		tmpExe.append(numero+1)
 
+	entradas = list(tmpEnt)
+	tempos = list(tmpExe)
+	for j in range(0,n): #ORDENA AS OS TEMPOS COM BASE NO TEMPO DE PROCESSO (DO MENOR PARA O MAIOR)
+		for i in range(0,n-1):
+			if tempos[i]>tempos[i+1]: 
+				Aux = tempos[i+1] 	#TROCA O TEMPO
+				tempos[i+1] = tempos[i]
+				tempos[i] = Aux
+				Aux = entradas[i+1] #TROCA A ENTRADA
+				entradas[i+1] = entradas[i]
+				entradas[i] = Aux	
+	soma = 0
+	relogio = 0
+	for x in xrange(0,n):
+		for y in xrange(0,n):
+			if entradas[y] <= relogio and entradas[y]>=0:
+				escolha = y
+				break
+			pass
+		relogio += tempos[escolhido] #INCREMENTA O RELOGIO COM O TEMPO DE EXECUCAO DO PROCESSO
+		soma += relogio - entradas[escolhido]
+		entradas[escolhido]=-1
+		final = soma/n
+		arquivo = open('SJFF.txt', 'w')#ABRE O ARQUIVO 'SJF.txt' COM PERMISSAO DE LEITARA E ESCRITA
+		arquivo.write("%s\n" %final)#ESCREVE TUDO O QUE ESTA EM 'contador' E 'item'
+		arquivo.close()#FECHA O ARQUIVO
+		arquivo = open('SJFF.txt', 'r')#ABRE O ARQUIVO 'STF.txt' COM PERMISSAO DE LEITURA
+		leitura = arquivo.read()#LE TUDO QUE ESTA NO ARQUIVO
+		LBL_Leituraa['text'] = ("%su.t"%leitura)#TROCA A CHAVE 'text' DO LABEL 'LBL_Leitura'
+		arquivo.close()#FECHA O ARQUIV		
+			
 #############LABELS	
 #NUMERO DE PROCESSOS
 LBL_PRO = Label(Main,text = 'Numero de Processos:')#NOME
 LBL_PRO['bg'] = 'white'#COR
-LBL_PRO.place(x = 0,y = 10)#LOCAL X,Y
+LBL_PRO.place(x = 7,y = 10)#LOCAL X,Y
 LBL_PRO['font'] = FONT2#FONTE
 
 #ENTRADA DE DADOS
-ETY_PRO = Entry(Main,width = 13)#ENTRADA
-ETY_PRO.insert(END,'1-46')#TEXTO DETRO
+ETY_PRO = Entry(Main,width = 12)#ENTRADA
+ETY_PRO.insert(END,'1')#TEXTO DETRO
 ETY_PRO['font'] = FONT2#FONTE
 ETY_PRO.place(x = 180,y = 10)#LOCAL X,Y
 
 #ESCALONAMENTO SJF
-LBL_SJF = Label(Main,text = 'Escalonamento SJF',font = FONT1,bg = 'white',fg = 'red').place(x = 10,y = 80)#NOME
+LBL_SJF = Label(Main,text = 'Escalonamento SJF',font = FONT1,bg = 'white',fg = 'red').place(x = 10,y = 70)#NOME
 
 #LINHA DE DIVISAO
 LBL_Linha = Label(Main,width = 90,bg = 'red').place(x = 0, y = 125,height = 0)#LINHA COM LOCAL X,Y
@@ -94,28 +152,21 @@ LBL_Developers = Label(Main,text = 'Karan Luciano | Jackson Duraes',bg = 'white'
 LBL_Developers.place(x = 165, y = 115)#LOCAL X,Y
 
 #PROCESSOS | CPUBUSTER
-LBL_Open = Label(Main,text = 'Processo | CPUBuster',font = FONT1,bg = 'white')#NOME
-LBL_Open.place(x = 100, y = 140)#LOCAL X,Y
+LBL_Open = Label(Main,text = 'Processo | CPUBuster      Tempo Medio',font = FONT1,bg = 'white')#NOME
+LBL_Open.place(x = 39, y = 140)#LOCAL X,Y
 
 #ESPACO EM BRANCO DE LEITURA
 LBL_Leitura = Label(Main, text='', bg='white')#ESPACO EM BRANCO
-LBL_Leitura.place(x=120, y=160)#LOCAL X,Y
+LBL_Leitura.place(x=60, y=160)#LOCAL X,Y
 
+LBL_Leituraa = Label(Main, text='', bg='white',font = FONT1)#ESPACO EM BRANCO
+LBL_Leituraa.place(x=260, y=175)#LOCAL X,Y
 
 #############BOTOES
 #BOTAO 'Gerar'
-BTO_Scan = Button(Main,text = 'Gerar',width = 5,height = 2)#NOME
-BTO_Scan['command'] = Scan#ATRABUI O COMANDO BOTAO, CHAMANDO A FUNCAO 'Scan' 
+BTO_Scan = Button(Main,text = 'GERAR',width = 7,height = 3)#NOME
+BTO_Scan['command'] = Scan#,SJF#ATRABUI O COMANDO BOTAO, CHAMANDO A FUNCAO 'Scan' 
 BTO_Scan['font'] = FONT1#TIPO DE FONTE
-BTO_Scan.place(x = 315, y = 10)#LOCAL X,Y
-
-#BOTAO 'Exibir'
-BTO_Exib = Button(Main,text = 'Exibir',width = 5,height = 2)#NOME
-BTO_Exib['command'] = Lei#ATRABUI O COMANDO BOTAO, CHAMANDO A FUNCAO 'Lei'
-BTO_Exib['font'] = FONT1#TIPO DE FONTE
-BTO_Exib.place(x = 315, y = 70)#LOCAL X,Y
+BTO_Scan.place(x = 300, y = 10)#LOCAL X,Y
 
 Main.mainloop()#CHAMA Tk
-
-
-#
