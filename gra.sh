0e78e1613f2226e307fe9118f085a81bdca78bee
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2019-05-03 03:17:28 -0400

    Add files via upload

---
+++ gra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gnuplot -persist <<-EOFMarker
+	set key above
+	set xlabel 'PROCESSO'
+	set ylabel 'TURNAROUND / WAITING / BUSTER'
+	set terminal wxt  enhanced title "Turnaround" persist raise
+    plot "/home/karan/Documentos/Operacionais/turnaround.txt" title 'Tournaround (roxo)' with lines smooth csplines, 'waiting.txt' title 'Waiting (verde)' with lines smooth csplines, 'buster.txt' title 'Buster (azul)' with lines smooth csplines
+EOFMarker
+
+#CENTRALIZA OS TITLES
+#LABEL DO EIXO X
+#LABEL DO EIXO Y
+#TITULO DA JANELA
+#FAZ AS LINHAS DE ACORDO COM OS VALORES INFORMADOS NOS ARQUIVOS
