9f08b33c7ccae6b87a1d59af45b580148a190b3a
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2019-05-03 03:16:30 -0400

    Update turnaround.sh

--- turnaround.sh
+++ turnaround.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
   
-DADOS=/home/karan/Documentos/operacionais/turnn.txt
+DADOS=/home/karan/Documentos/Operacionais/turnaround.txt
 N=$(wc -l $DADOS | cut -d " " -f 1)
 SOMA=$(paste -sd+ $DADOS | bc)
-MEDIA=$(echo "scale=2; $SOMA/$N" | bc)
+MEDIA=$(echo "scale=1; $SOMA/$N" | bc)
 echo "Average Turnaround: $MEDIA" 
+
+#wc -l CONTA O NUMERO DE LINHAS NO ARQUIVO, cut faz o tratamento para sobrar apenas o digito
+#FAZ A SOMA DE TODOS OS VALORES NO ARQUIVO
+#FAZ O CALCULO DA MEDIA COM ESCALA DE CASA DECIMAIS
+#MOSTRA A MEDIA 
