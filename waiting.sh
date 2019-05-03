71ff85518e9ffd35509c1328edc104e7e71babe1
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2019-05-03 03:29:15 -0400

    Update waiting.sh

--- waiting.sh
+++ waiting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   
-DADOS=/home/karan/Documentos/Operacionais/waiting.txt
+DADOS=waiting.txt
 N=$(wc -l $DADOS | cut -d " " -f 1)
 SOMA=$(paste -sd+ $DADOS | bc)
 MEDIA=$(echo "scale=2; $SOMA/$N" | bc)
